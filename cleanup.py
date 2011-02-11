7f57d2f13471a20000b6d1cfe59d8c4ec98adb07
Daniel Thompson <dan@d4nt.me.uk>
2011-02-11 20:58:55 +0000

    Added cleanup script

---
+++ cleanup.py
@@ -0,0 +1,17 @@
+import sqlite3
+from settings import *
+
+def isSchemaThere(conn):
+	cur = conn.cursor()
+	cur.execute("SELECT name FROM sqlite_master WHERE name='links'")
+	if len(cur.fetchall()) > 0:
+		return True
+	return False
+
+conn = sqlite3.connect(LOCAL_LINK_DB)
+if isSchemaThere(conn) == True:
+    cur = conn.cursor()
+    cur.execute("DELETE FROM links WHERE first_seen < datetime('now', '-7 day')")
+    conn.commit()
+    conn.close()
+
